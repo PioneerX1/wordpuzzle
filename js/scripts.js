69ffe9c461eede02e4aa3548ff886ac4bf75ee62
Diego Aguilar and Mick Sexton <mick@>
2017-06-06 11:23:11 -0700

    add fix for loop issue and switching back and forth between show and hide

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
   $("form#text-form").submit(function(event) {
     var inputString = $("input#text").val();
-    //alert(inputString);
+    //alert(inputString + " " + inputString.length);
     var outputArray = [];
+    var outputString = "";
 
-    for (var i=0; i <= inputString.length; i++) {
-      if (inputString.chatAt(i) === "e") {
-        outputArray[i] = "-";
-      } else {
-        outputArray[i] = inputString.charAt(i);
+    for (var i=0; i < inputString.length; i+=1) {
+      outputArray[i] = inputString.charAt(i);
+    }
+
+    for (var i=0; i < outputArray.length; i++) {
+      if (outputArray[i] === 'e' || outputArray[i] === 'a' || outputArray[i] === 'i' || outputArray[i] === 'o' || outputArray[i] === 'u') {
+        outputArray[i] = '-';
       }
     }
-    alert(outputArray.toString());
+
+    for (var i=0; i < outputArray.length; i++) {
+      outputString = outputString.concat(outputArray[i]);
+    }
+    //alert(outputString);
+    //alert(outputArray.toString());
+    $("#phrase").text(outputString);
+    $("#text-form").hide();
+    $("#puzzle").show();
 
     event.preventDefault();
+  });
+  $("#backButton").click(function() {
+    $("#text-form").show();
+    $("#puzzle").hide();
   });
 });
